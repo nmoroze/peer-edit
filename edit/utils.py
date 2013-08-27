c3932fa59e0818d4cd6359e6140c11e7f4222e02
Noah Moroze <noahm@moroze.com>
2013-08-26 21:35:55 -0400

    Added notification on feedback submission

--- edit/utils.py
+++ edit/utils.py
@@ -5,9 +5,11 @@ from django.http import HttpResponse
 def save(request, paper_id):
 	paper = Paper.objects.get(id=paper_id)
 	paper.body = request.POST['data']
-	f = Feedback(content=request.POST['content'], paper=paper, author=Author.objects.get(id=request.POST['author']))
-	print f
-	sys.stdout.flush()
+	author = Author.objects.get(id=request.POST['author'])
+	f = Feedback(content=request.POST['content'], paper=paper, author=author)
 	f.save()
 	paper.save()
+	notification = Notification(content=author.user.username+" submitted feedback on "+paper.question, author=paper.author)
+	notification.save()
+
 	return HttpResponse("success!")
