dce4af7a53fec1bf8dfcc34842de16939b69cedc
Noah Moroze <noahm@moroze.com>
2013-08-27 23:45:16 -0400

    Tentatively going to declare deletions as working

--- edit/utils.py
+++ edit/utils.py
@@ -5,11 +5,18 @@ from django.http import HttpResponse
 def save(request, paper_id):
 	paper = Paper.objects.get(id=paper_id)
 	paper.body = request.POST['data']
+	paper.save()
+	return HttpResponse("Success!")
+
+def feedbacksave(request, paper_id):
+	paper = Paper.objects.get(id=paper_id)
 	author = Author.objects.get(id=request.POST['author'])
 	f = Feedback(content=request.POST['content'], paper=paper, author=author)
 	f.save()
-	paper.save()
 	notification = Notification(content=author.user.username+" submitted feedback on "+paper.question, author=paper.author)
 	notification.save()
+	return HttpResponse(f.id)
 
-	return HttpResponse("success!")
+def delete(request):
+	Feedback.objects.get(id=request.POST['id']).delete()
+	return HttpResponse("Success!")
